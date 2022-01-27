313a71b4fc5b91d5d21efcc48f6b4a4542359431
Clemens Horn <clemens.horn@mni.thm.de>
2022-01-27 09:24:12 +0100

    added discover-coffee-shops app to workspace
    Signed-off-by: Clemens Horn <clemens.horn@mni.thm.de>

---
+++ apps/discover-coffee-shops/pages/index.tsx
@@ -0,0 +1,20 @@
+import styles from './index.module.scss';
+
+export function Index() {
+    return (
+        <div className={styles.page}>
+            <div className="wrapper">
+                <div className="container">
+                    <div id="welcome">
+                        <h1>
+                            <span> Hello there, </span>
+                            Welcome to discover-coffee-shops 👋
+                        </h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Index;
